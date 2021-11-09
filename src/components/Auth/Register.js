3463de1ff2387e58a85aa5cecc7ad5fea5358a00
macbright <brightokike@gmail.com>
2021-11-09 13:11:21 +0300

    finished the sign up page design

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from "react-router-dom";
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
+
+import { ReactComponent as Logo } from  '../../assets/logo.svg';
 import inputs from './inputs';
 import Slider from '../slider/Slider';
 import styles from './register.module.scss';
@@ -28,26 +31,37 @@ const Register = () => {
        <div className={styles.signupDiv}> 
         <Slider />
         <div className={styles.signupRight} >
+            <h1> Let's get started </h1>
+            <p className={styles.subheading}>Already have an account? <Link to="/login">Sign in</Link>
+ </p>
+
             <form  onSubmit={handleSubmit(onSubmit)}>
                 {
                     inputs.map((input) =>
                         (
-                            <div  key={input.label}>
+                           <div className={styles.inputUpperDiv}> <div  key={input.label} className={styles.inputDiv}>
                                 <label >{input.label}</label>
                                 <input
                                     type={input.type}
                                     name={input.name}
                                     placeholder={`Enter your ${input.name}`}
                                     {...register(input.name)} 
-                                />
-                                <p>{ errors[input.name]?.message}</p>
-                            </div>        
+                                    className={ errors[input.name] ? styles.errorField : '' }
+                                /> 
+                                <p className={styles.error}>{ errors[input.name]?.message}</p>
+                            </div>  </div>      
                         )
 
                     )
                 }
-                <button  type="submit"> submit</button>
+                <div className={styles.termService}>
+                    <input type="checkbox" />
+                    <label >I agree to <a> Terms of Use</a> and  <a> Privacy Policy </a> </label>
+                </div>
+
+                <button  type="submit" className={styles.createBtn}> Create Account </button>
             </form>
+            <Logo  className={styles.logo}/>
         </div>
         
        </div>
